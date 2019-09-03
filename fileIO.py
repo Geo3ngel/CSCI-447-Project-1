864c8c15ffcfdb021a268c73eb71030e90f0cffe
GeoEngel <GeoEngel.z@gmail.com>
2019-09-03 10:30:58 -0600

    Added file text comma parsing with '\n' removal.

--- fileIO.py
+++ fileIO.py
@@ -1,11 +1,18 @@
 
 # Loads the file entirly into memory for processing
 def process_database_file(file_name):
-    file = open(file_name, "r+")
+    file = open(file_name, "r")
     # TODO: Change to use numpy for efficiency's sake?
     database = []
     for set in file:
-        # TODO: break up set into an actual set of the values, don't leave it as a string!
-        database.append(set)
+        # Converts the string line from the file into a set of values.
+        database.append(string_to_set(set))
+            
     file.close()
     return database
+
+# Converts comma seperated strings into a set of values.
+def string_to_set(value):
+    value = value.strip('\n')
+    value_set = value.split(",")
+    return value_set
