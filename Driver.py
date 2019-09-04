f7515c4e5b75ec9415b4166e97a341e2653658be
GeoEngel <GeoEngel.z@gmail.com>
2019-09-03 20:26:57 -0600

    Added data correction method which currently just removes unfinished set

--- Driver.py
+++ Driver.py
@@ -1,6 +1,7 @@
 import processData
 
 print("Starting...")
-db = processData.process_database_file("databases/breast-cancer-wisconsin/wdbc.data")
-db.to_string()
+db = processData.process_database_file("databases/soybean/soybean-small.data")
+# db.to_string()
+processData.data_correction(db.get_data(), 36)
 print("Finished.")
