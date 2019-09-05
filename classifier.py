9c1b4ea1cf3967728fa12fb454bded7755b03dd5
Henry Soule <hsoule427@gmail.com>
2019-09-05 16:50:55 -0600

    Added function getCount and attributeCount

---
+++ classifier.py
@@ -0,0 +1,34 @@
+import fileIO
+
+def classifyData(attributes, data, attToClassify):
+    """
+    [[1,2,3],[1,2,3]]
+    """
+    # attributes: []
+    # data: [[1,2,3],[1,2,3]]
+    # attToClassify: "Foo", A string matching the column name, or attribute, of the dataset
+    # Creating a dictionary to hold the class types
+    dataClasses = {}
+    for i in range(len(data))
+        example = data[i]
+        if example[i] not in dataClasses:
+            data[example[i]] = []
+        dataClasses.append(example)
+    return dataClasses
+
+
+def attributeCount(subset): #TODO find a better name for this function
+    # acquire list of attributes
+    attributes = []
+    
+    for a in attributes:
+        getCount()
+
+
+def getCount(attribute, data): #TODO find a better name for this one as well
+    for row in data:
+         
+    
+        
+    
+
