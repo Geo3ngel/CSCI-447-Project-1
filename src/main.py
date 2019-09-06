25679d2f0e81697fdb8bf0ae125d9af0bad2053a
GeoEngel <GeoEngel.z@gmail.com>
2019-09-06 13:52:29 -0600

    Some printout clean up for the sake of my sanity.

--- src/main.py
+++ src/main.py
@@ -3,13 +3,28 @@
 @authors     George Engel, Troy Oster, Dana Parker, Henry Soule
 @brief       The file that runs the program
 """
-
 import process_data
 
+# Cleaner print outs for the sake of my sanity.
+def print_database(database):
+    
+    if len(database) < 1:
+        print("[] - Empty")
+    else:
+        for row in database:
+            print(row)
+
 print("Starting...")
-db = process_data.process_database_file("../databases/soybean/soybean-small.data")
-# db.to_string()
-normal_data, irregular_data = process_data.data_correction(db.get_data(), 36)
-print("Normal Data:\n" + str(normal_data))
-print("Irregular Data:\n" + str(irregular_data))
-print("Finished.")
+# TODO: Enter user input for pathing. -George
+# TODO: Make pathing manager for choosing database -George
+db = process_data.process_database_file("../databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data")
+
+normal_data, irregular_data = process_data.data_correction(db.get_data(), 11)
+
+print("\nNormal Data:")
+print_database(normal_data)
+
+print("\nIrregular Data:")
+print_database(irregular_data)
+
+print("\nFinished.")
