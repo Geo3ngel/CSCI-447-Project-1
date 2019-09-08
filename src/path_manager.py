43b0da4bac6715668c6cb9fd08f4b16b60b3e2e4
GeoEngel <GeoEngel.z@gmail.com>
2019-09-08 02:25:24 -0600

    Added useful methods for file/directory validation

--- src/path_manager.py
+++ src/path_manager.py
@@ -9,12 +9,40 @@ class pathManager:
         # Stores the root directory of the project.
         self.ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         # Initializes the folder name for the directory that holds the databases.
-        self.databases_dir = "databases"
+        self.databases_folder = "databases"
+        
+        self.update_databases_dir()
         
         print("Path manager Initialized!")
+        
+    def get_ROOT_DIR(self):
+        return self.ROOT_DIR
+        
+    def update_databases_dir(self):
+        # Joins the database folder in such a way that is supported on every OS.
+        self.databases_dir = os.path.join(self.ROOT_DIR, self.databases_folder)
         
     def get_databases_dir(self):
         return self.databases_dir
     
-    def set_databases_dir(self, folder_name):
-        self.databases_dir = folder_name
+    def set_databases_folder(self, folder_name):
+        self.databases_folder = folder_name
+        self.update_databases_dir()
+        
+    # Returns True if the file path entered is valid/non-empty
+    def validate_file(self, file_path):
+        if file_path is None:
+            return False
+        elif os.path.exists(file_path):
+            return True
+        else:
+            return False
+        
+    # Returns True si the directory path entered is valid/non-empty
+    def validate_dir(self, dir_path):
+        if dir_path is None:
+            return False
+        elif os.path.isdir(dir_path):
+            return True
+        else:
+            return False
