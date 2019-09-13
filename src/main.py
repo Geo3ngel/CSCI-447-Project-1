29f05dfa98ab0a000ff0cce417852e91dd8b48ae
GeoEngel <GeoEngel.z@gmail.com>
2019-09-13 03:14:43 -0600

    more optimizations

--- src/main.py
+++ src/main.py
@@ -56,8 +56,8 @@ db = process_data.process_database_file(path_manager)
 normal_data, irregular_data = process_data.identify_missing_data(db)
 
 corrected_data = process_data.extrapolate_data(normal_data, irregular_data, db.get_missing_symbol())
-print("\nNormal Data:")
-print_database(normal_data)
+# print("\nNormal Data:")
+# print_database(normal_data)
 
 # -------------------------------------------------------------
 
